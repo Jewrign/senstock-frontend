741f7d466ee60ce6fb77666d46117e096c91a1f5
jewrign <aliounebadarasene929@gmail.com>
2025-06-17 13:56:25 +0000

    Ajout d'image

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,17 +1,53 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import api from '../services/api';
+import axios from '../services/api';
 import BackButton from '../components/BackButton';
 
 export default function AddProduct() {
   const [form, setForm] = useState({ nom: '', categorie: '', stock: '', prix_unitaire: '', seuil_alerte: '' });
+  const [image, setImage] = useState(null);
+  const [preview, setPreview] = useState(null);
+  const [message, setMessage] = useState('');
   const navigate = useNavigate();
 
   const handleChange = e => setForm({ ...form, [e.target.name]: e.target.value });
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+    setPreview(URL.createObjectURL(file));
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await api.post('/produits', form);
+
+    const data = new FormData();
+    Object.entries(form).forEach(([key, value]) => {
+      data.append(key, value);
+    });
+    if (image) {
+      data.append('image', image);
+    }
+
+    try {
+        await axios.post('/produits', data, {
+          headers: { 'Content-Type': 'multipart/form-data' },
+        });
+        setMessage('✅ Produit ajouté avec succès');
+        setForm({
+          nom: '',
+          categorie: '',
+          description: '',
+          stock: '',
+          prix_unitaire: '',
+          seuil_alerte: '',
+        });
+        setImage(null);
+        setPreview(null);
+      } catch (error) {
+        setMessage('❌ Erreur lors de l’ajout');
+        console.error(error);
+      }
     navigate('/produits');
   };
 
@@ -23,7 +59,11 @@ export default function AddProduct() {
       {['nom', 'categorie', 'stock', 'prix_unitaire', 'seuil_alerte'].map(field => (
         <input key={field} name={field} placeholder={field} onChange={handleChange} className="w-full p-2 border rounded" />
       ))}
+      <input type="file" accept="image/*" onChange={handleImageChange} className="w-full border p-2" />
+
+        {preview && <img src={preview} alt="Aperçu" className="h-32 mt-2 object-cover rounded" />}
       <button className="bg-blue-600 text-white px-4 py-2 rounded">Enregistrer</button>
+      {message && <p className="mt-4 text-center">{message}</p>}
     </form>
     </div>
   );
