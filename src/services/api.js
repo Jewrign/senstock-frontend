db5b0e622fa093c4f4fd19e4f623f755096bc35d
jewrign <aliounebadarasene929@gmail.com>
2025-06-23 18:49:41 +0000

    Modification de api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://senstock-backend.onrender.com',
+  baseURL: 'https://senstock-backend.onrender.com/api',
   withCredentials: true,
   headers: {
     'Accept': 'application/json',
